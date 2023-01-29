cde557880b00cfecf1cfee6dbb60d403c7ac3eea
kurosawaryouta <crow0210kuro@gmail.com>
2023-01-29 13:58:18 +0900

    画像ビルドテスト

--- konpeitou_pj_react/src/component/konpeitou/Logo/index.tsx
+++ konpeitou_pj_react/src/component/konpeitou/Logo/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React, { useEffect } from "react";
 import { useTimer } from "use-timer";
 
+import LogoImage from "../../../../public/image/others/コンペイトウミュージアム-ロゴ.png";
 import CSS from "../konpeitou.module.css";
 
 export const Logo: React.FC = () => {
@@ -19,13 +20,6 @@ export const Logo: React.FC = () => {
     css_templete = CSS.Logo_delay_opacity_0;
   }
 
-  // console.log(location.hostname);
-
-  // var hostname = location.hostname;
-  // const myLoader = ({ src }: { src: string }) => {
-  //   return `https://${hostname}/${src}`;
-  // };
-
   return (
     <>
       <div
@@ -37,11 +31,7 @@ export const Logo: React.FC = () => {
           `${time > 2.0 && CSS.Logo_delay_opacity_0}`
         }
       >
-        <Image
-          src="/image/others/コンペイトウミュージアム-ロゴ.png"
-          alt="Picture of the author"
-          fill
-        />
+        <Image src={LogoImage} alt="Picture of the author" fill />
       </div>
 
       {/* <div
